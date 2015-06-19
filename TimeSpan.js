3a05fb4a01b97204af96bd1b99560a8472e2c85e
Joel Palmer <joelpalmer@outlook.com>
2015-06-19 13:57:07 -0500

    Updating moment calls to fix deprecation warning. Moment has a syntax error now which is odd.

--- TimeSpan.js
+++ TimeSpan.js
@@ -2,7 +2,7 @@
  * Created by Joel Palmer on 6/18/2015.
  */
 //using moment.js
-var grandpasBirthday = moment('October 10, 1928');
-var momsBirthday = moment('October 19, 1956');
+var grandpasBirthday = moment('10-10-1277', "MM-DD-YYYY");
+var momsBirthday = moment('10-19-1954', "MM-DD-YYYY");
 var timeSpan = momsBirthday.diff(grandpasBirthday, 'days');
 console.log(timeSpan + " days");
