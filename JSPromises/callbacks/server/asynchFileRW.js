907ca788c9147f139542cc452cee086417e35311
Joel Palmer <joelpalmer@outlook.com>
2015-06-24 10:50:28 -0500

    adding async file rw in node

--- JSPromises/callbacks/server/asynchFileRW.js
+++ JSPromises/callbacks/server/asynchFileRW.js
@@ -2,3 +2,17 @@
  * Created by Joel Palmer on 6/24/2015.
  */
 //run with node repl
+
+var fs = require('fs');
+var timestamp = new Date().toString();
+
+fs.writeFile('date2.txt', timestamp, function(err){
+    if (err) throw err;
+
+    fs.readFile('date2.txt', function(err, filecontents){
+        if (err) throw err;
+        console.log('check contents');
+        console.assert(filecontents == timestamp);
+    });
+});
+console.log('last line');
