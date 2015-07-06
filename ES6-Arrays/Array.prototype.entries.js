d06504a67b5553fe167186f66ee74f42be4fe21f
Joel Palmer <joelpalmer@outlook.com>
2015-07-06 10:39:36 -0500

    Array.prototype.entries

---
+++ ES6-Arrays/Array.prototype.entries.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Joel Palmer on 7/6/2015.
+ */
+
+var dodgersArray = ["Pederson", "Puig", "Greinke", "Kershaw", "Turner"];
+var daEntries = dodgersArray.entries();
+
+console.log(daEntries.next().value);
