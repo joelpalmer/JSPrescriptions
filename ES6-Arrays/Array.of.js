ed98767f6d3ce113e338729df03d9b72fc93f79a
Joel Palmer <joelpalmer@outlook.com>
2015-07-06 09:31:26 -0500

    Array.of (only working in FF despite MDN docs saying Chrome supports it with experimental JS flag set)

---
+++ ES6-Arrays/Array.of.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Joel Palmer on 7/6/2015.
+ *
+ * ES6
+ * Must have experimental js enabled in Chrome 39+. Also works...
+ * in FF 25+ (This is not working in Chrome 43 with flag set as of now
+ * Polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/of
+ *
+ */
+
+var dodgersArray = Array.of("Joc Pederson", "Zack Greinke", "Justin Turner");
+console.log(dodgersArray);
+
+
+
+
