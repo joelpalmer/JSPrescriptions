617ef42a894c7733c89e203e873d003b4782109f
Joel Palmer <joelpalmer@outlook.com>
2015-07-06 10:51:06 -0500

    Array.prototype.fill

---
+++ ES6-Arrays/Array.prototype.fill.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Joel Palmer on 7/6/2015.
+ *
+ * This does not work in latest Chrome with flag set for me
+ */
+var dodgersSSArray = ["Rollins", "Rollins", "Rollins"];
+console.log(dodgersSSArray);
+dodgersSSArray.fill("Seager");
+console.log(dodgersSSArray);
