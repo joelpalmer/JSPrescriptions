831e0051a4214ec959993c5d72560f1e95e86acc
Joel Palmer <joelpalmer@outlook.com>
2015-07-06 09:59:42 -0500

    Note that I updated MDN

--- ES6-Arrays/Array.of.js
+++ ES6-Arrays/Array.of.js
@@ -2,8 +2,8 @@
  * Created by Joel Palmer on 7/6/2015.
  *
  * ES6
- * Must have experimental js enabled in Chrome 39+. Also works...
- * in FF 25+ (This is not working in Chrome 43 with flag set as of now
+ * Must have experimental js enabled in Chrome 45+. Also works...
+ * in FF 25+ (I updated MDN docs from  Chrome 39 to 45)
  * Polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/of
  *
  */
