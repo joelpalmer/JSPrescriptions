97805ddd03f201c3d7c49d2ee6ad4251d05a73af
Joel Palmer <joelpalmer@outlook.com>
2015-07-06 11:16:28 -0500

    Same example code as find() but with findIndex()

---
+++ ES6-Arrays/Array.prototype.findIndex.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Joel Palmer on 7/6/2015.
+ *
+ * ES6 - FF only as of now
+ *
+ * Polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+ *
+ * Same example code as find() but with findIndex()
+ */
+
+function isEven(el, idx, array){
+    return el % 2 == 0;
+}
+
+var test = 3;
+var test2 = 2;
+
+console.log(isEven(test));
+console.log(isEven(test2));
+
+var nums = [3, 9, 11, 14];
+
+console.log(nums.findIndex(isEven));
+
+console.log(nums.findIndex(function (el, idx, array){
+    return el % 2 == 0;
+}));
+
+
+
+
